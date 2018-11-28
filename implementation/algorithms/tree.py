d51918b9d5395fb895d9cdfcc4b49ecea60d4d72
Alina <alina.zamfir_1997@yahoo.com>
2018-11-28 22:27:07 +0200

    Tree representation.
    Signed-off-by: Alina <alina.zamfir_1997@yahoo.com>

---
+++ implementation/algorithms/tree.py
@@ -0,0 +1,26 @@
+
+class TreeNode:
+    def __init__(self,
+                 parent = None,
+                 value = None,
+                 children = None):
+
+        self.parent = parent
+        self.value = value
+
+        if isinstance(children, dict):
+            self.children = children
+        else:
+            self.children = {}
+
+
+if __name__ == '__main__':
+    root = TreeNode(value=2)
+    root.children['move_1'] = TreeNode(parent= root, value=5)
+    root.children['move_2'] = TreeNode(parent= root, value= 5)
+    root.children['move_1'].children['move_1'] = TreeNode(parent= root.children['move_1'], value=10)
+
+    print(root)
+    print(root.children)
+    print(root.children['move_1'].children)
+
