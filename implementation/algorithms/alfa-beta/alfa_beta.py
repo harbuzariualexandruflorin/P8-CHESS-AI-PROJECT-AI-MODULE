b68162268b05e953ec1b161fd36b37fea925d622
Lazaroi <lazaroidarius@yahoo.com>
2018-12-02 14:58:22 +0200

    more changes

--- implementation/algorithms/alfa-beta/alfa_beta.py
+++ implementation/algorithms/alfa-beta/alfa_beta.py
@@ -2,8 +2,8 @@ import implementation.algorithms.tree.tree as tree
 
 #typecheck this
 def alpha_beta(root,turns_predicted):
-    alpha = -9999999
-    beta = 9999999
+    alpha = -1
+    beta = 1
     result_node = alpha_beta_recursive(root, alpha, beta, True)
     parent_node = result_node
     for i in range(0, turns_predicted*2-2):
@@ -19,7 +19,7 @@ def alpha_beta_recursive(node, alpha, beta, maximizing_player):
         return node
 
     if maximizing_player:
-        max_eval = tree.TreeNode(value=-999999)
+        max_eval = tree.TreeNode(value=-1)
         for key, value in node.children.items():
             eval_node = alpha_beta_recursive(node.children[key], alpha, beta, False)
             eval = eval_node.value
@@ -31,7 +31,7 @@ def alpha_beta_recursive(node, alpha, beta, maximizing_player):
         return max_eval
 
     else:
-        min_eval = tree.TreeNode(value=999999)
+        min_eval = tree.TreeNode(value=1)
         for key, value in node.children.items():
             eval_node = alpha_beta_recursive(node.children[key], alpha, beta, True)
             eval = eval_node.value
@@ -52,14 +52,14 @@ if __name__=='__main__':
     root.children['move_1'].children['move_4'] = tree.TreeNode(parent=root.children['move_1'])
     root.children['move_2'].children['move_5'] = tree.TreeNode(parent=root.children['move_2'])
     root.children['move_2'].children['move_6'] = tree.TreeNode(parent=root.children['move_2'])
-    root.children['move_1'].children['move_3'].children['move_7'] = tree.TreeNode(parent=root.children['move_1'].children['move_3'], value=-1)
-    root.children['move_1'].children['move_3'].children['move_8'] = tree.TreeNode(parent=root.children['move_1'].children['move_3'], value=3)
-    root.children['move_1'].children['move_4'].children['move_9'] = tree.TreeNode(parent=root.children['move_1'].children['move_4'], value=5)
-    root.children['move_1'].children['move_4'].children['move_10'] = tree.TreeNode(parent=root.children['move_1'].children['move_4'], value=1)
-    root.children['move_2'].children['move_5'].children['move_11'] = tree.TreeNode(parent=root.children['move_2'].children['move_5'], value=-6)
-    root.children['move_2'].children['move_5'].children['move_12'] = tree.TreeNode(parent=root.children['move_2'].children['move_5'], value=-4)
+    root.children['move_1'].children['move_3'].children['move_7'] = tree.TreeNode(parent=root.children['move_1'].children['move_3'], value=-0.1)
+    root.children['move_1'].children['move_3'].children['move_8'] = tree.TreeNode(parent=root.children['move_1'].children['move_3'], value=0.3)
+    root.children['move_1'].children['move_4'].children['move_9'] = tree.TreeNode(parent=root.children['move_1'].children['move_4'], value=0.5)
+    root.children['move_1'].children['move_4'].children['move_10'] = tree.TreeNode(parent=root.children['move_1'].children['move_4'], value=0.1)
+    root.children['move_2'].children['move_5'].children['move_11'] = tree.TreeNode(parent=root.children['move_2'].children['move_5'], value=-0.6)
+    root.children['move_2'].children['move_5'].children['move_12'] = tree.TreeNode(parent=root.children['move_2'].children['move_5'], value=-0.4)
     root.children['move_2'].children['move_6'].children['move_13'] = tree.TreeNode(parent=root.children['move_2'].children['move_6'], value=0)
-    root.children['move_2'].children['move_6'].children['move_14'] = tree.TreeNode(parent=root.children['move_2'].children['move_6'], value=9)
+    root.children['move_2'].children['move_6'].children['move_14'] = tree.TreeNode(parent=root.children['move_2'].children['move_6'], value=0.9)
 
 
     #print(root)
