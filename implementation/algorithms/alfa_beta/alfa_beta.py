17e5e39d99d6e5d363799294c0753674118c7a10
Lazaroi <lazaroidarius@yahoo.com>
2018-12-10 11:08:17 +0200

    mistake checker & alfa-beta finishings.

--- implementation/algorithms/alfa_beta/alfa_beta.py
+++ implementation/algorithms/alfa_beta/alfa_beta.py
@@ -1,24 +1,69 @@
 import implementation.algorithms.tree.tree as tree
+import implementation.algorithms.tree.tree_gen as construction
+import utils.chess_utils as evaluation
+
+
+import chess
 
 from typing import *
 from typeguard import typechecked
+
+
+
+#only takes like half a fucking minute to run
 
 @typechecked
 def alpha_beta(root: tree.TreeNode) -> str:
     alpha = -1
     beta  = 1
+    move_list = list()
 
     parent_node = alpha_beta_recursive(root, alpha, beta, True)
     parent_test = parent_node.parent
 
-    while parent_test.parent:
-        parent_node = parent_node.parent
-        parent_test = parent_test.parent
+    while True:
+        for key, value in parent_test.children.items():
+            if parent_test.children[key] == parent_node:
+                move_list.insert(0, key)
+        if parent_test.parent:
+            parent_node = parent_node.parent
+            parent_test = parent_test.parent
+        else:
+            break
+
+    turns_to_end_game = find_end_game(move_list)
 
     for key, value in root.children.items():
         if root.children[key] == parent_node:
-            return key
+            string = str(turns_to_end_game) + " " +str(key)
+            return string
 
+
+
+
+def find_end_game(move_list):
+    board = chess.Board()
+    board = construction.construct_current_state(board, move_list)
+
+    turns_to_endgame = 0
+
+    while not board.is_game_over() and turns_to_endgame < 150:
+        move_list.clear()
+        max_gain = -1
+        best_move = ""
+
+        for move in board.legal_moves:
+            current_gain = evaluation.evaluate_board_state(board, str(move))
+            if max_gain <= current_gain:
+                max_gain = current_gain
+                best_move = str(move)
+
+        move_list.insert(0, best_move)
+        turns_to_endgame = turns_to_endgame+1
+        current_move = chess.Move.from_uci(best_move)
+        board.push(current_move)
+
+    return turns_to_endgame
 
 @typechecked
 def alpha_beta_recursive(node: tree.TreeNode,
@@ -63,7 +108,10 @@ def alpha_beta_recursive(node: tree.TreeNode,
         return min_eval
 
 
+
+
 if __name__=='__main__':
+    """
     root = tree.TreeNode()
     root.children['move_1'] = tree.TreeNode(parent=root)
     root.children['move_2'] = tree.TreeNode(parent=root)
@@ -79,9 +127,10 @@ if __name__=='__main__':
     root.children['move_2'].children['move_5'].children['move_12'] = tree.TreeNode(parent=root.children['move_2'].children['move_5'], value=-0.4)
     root.children['move_2'].children['move_6'].children['move_13'] = tree.TreeNode(parent=root.children['move_2'].children['move_6'], value=0)
     root.children['move_2'].children['move_6'].children['move_14'] = tree.TreeNode(parent=root.children['move_2'].children['move_6'], value=0.9)
-
+    """
 
     #print(root)
     #print(root.children)
     #print(root.children['move_1'].children)
-    print(alpha_beta(root))
+    move_list = ["g1h3"]
+    print(alpha_beta(construction.tree_gen(move_list, 1)))
--- implementation/algorithms/tree/tree_gen.py
+++ implementation/algorithms/tree/tree_gen.py
@@ -15,7 +15,7 @@ def tree_gen(move_list, predicted_turns):
     root = tree_node.TreeNode()
 
     for move in beginning_state.legal_moves:
-        evalued_value=evaluation.evaluate_board_state(beginning_state,str(move))
+        evalued_value = evaluation.evaluate_board_state(beginning_state,str(move))
         root.children[str(move)] = tree_node.TreeNode(parent=root,value=evalued_value)
         tree_generation_recursive(1, root.children[str(move)], beginning_state.copy(), max_predicted_moves)
 
@@ -24,7 +24,7 @@ def tree_gen(move_list, predicted_turns):
 
 #typecheck me
 def tree_generation_recursive(current_predicted_moves, current_node, beginning_state, max_predicted_moves):
-    if current_predicted_moves<max_predicted_moves:
+    if current_predicted_moves < max_predicted_moves:
         move_list = list()
         parent = current_node
 
@@ -34,9 +34,9 @@ def tree_generation_recursive(current_predicted_moves, current_node, beginning_s
                 if parent.children[key] == current_node:
                     move_list.append(str(key))
 
-        current_state = construct_current_state(beginning_state,move_list).copy()
+        current_state = construct_current_state(beginning_state, move_list).copy()
 
-        for move in current_state.generate_legal_moves():
+        for move in current_state.legal_moves:
             evalued_value                    = evaluation.evaluate_board_state(current_state, str(move))
             current_node.children[str(move)] = tree_node.TreeNode(parent=current_node,value=evalued_value)
             tree_generation_recursive(current_predicted_moves+1, current_node.children[str(move)], beginning_state.copy(), max_predicted_moves)
@@ -54,17 +54,110 @@ def construct_current_state(board, move_list):
     return board
 
 
-if __name__ == '__main__':
-    move_list=["g1h3"]
-    print(tree_gen(move_list, 1))
+def construct_current_state1(board, move_list):
+    i = 0
 
+    while i < len(move_list):
+        current_move = chess.Move.from_uci(move_list[i])
+        board.push(current_move)
+        i = i+1
+
+    return board
+
+
+def mistake_checker(move_list, player_color, mistake_threshold):
+    if len(move_list) < 5:
+        print("Not enough moves to check for mistakes")
+        return 0
 
     board = chess.Board()
+    player = 0
+    if player_color == "WHITE":
+        player = 1
+        board = construct_current_state1(board.copy(), move_list[:2])
+        move_list.pop(0)
+        move_list.pop(0)
+    elif player_color == "BLACK":
+        player = 2
+        board = construct_current_state1(board.copy(), move_list[:3])
+        move_list.pop(0)
+        move_list.pop(0)
+        move_list.pop(0)
 
 
-    for move in board.legal_moves:
-        board_copy=board.copy()
-        board_copy.push(move)
-        print(board_copy)
+    made_move_value_2 = 0
+    made_move_2=""
+    made_move_value_1 = 0
+    made_move_1 = ""
 
+
+    while True:
+        max_move_value_2 = -1
+        max_move_2 = ""
+        max_move_value_1 = -1
+        max_move_1 = ""
+        board_copy = board.copy()
+
+        made_move_1 = move_list[0]
+        made_move_value_1 = evaluation.evaluate_board_state(board_copy.copy(), move_list[0])
+        board_copy = construct_current_state1(board_copy.copy(), move_list[:1])
+        made_move_2 = move_list[1]
+        made_move_value_2 = evaluation.evaluate_board_state(board_copy.copy(), move_list[1])
+
+        board_copy = board.copy()
+
+        for move in board_copy.legal_moves:
+            current_move_value = evaluation.evaluate_board_state(board_copy.copy(), str(move))
+            current_move = str(move)
+            if current_move_value > max_move_value_1:
+                max_move_value_1 = current_move_value
+                max_move_1 = str(current_move)
+
+        sample_list=list()
+        sample_list.append(max_move_1)
+        board_copy = construct_current_state1(board.copy(), sample_list)
+
+        for move in board_copy.legal_moves:
+            current_move_value = evaluation.evaluate_board_state(board_copy.copy(), str(move))
+            current_move = str(move)
+            if current_move_value > max_move_value_2:
+                max_move_value_2 = current_move_value
+                max_move_2 = str(current_move)
+
+
+        if made_move_value_1 <= max_move_value_1 - mistake_threshold:
+            if made_move_1 != max_move_1:
+                if made_move_value_2 >= max_move_value_2:
+                    print("possible mistake at ",made_move_1,"\n")
+                else: print("questionable move at ",made_move_1,"\n")
+
+        if len(move_list)-2 == 0:
+            break
+        else:
+            board = construct_current_state1(board.copy(), move_list[:2])
+            move_list.pop(0)
+            move_list.pop(0)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    move_list = ["g1h3","d7d6","b2b3","h7h6","b3b4"]
+    #print(tree_gen(move_list, 1))
+    print(mistake_checker(move_list,"BLACK",0))
+
+    #board = chess.Board()
+
+
+    #for move in board.legal_moves:
+    #    board_copy = board.copy()
+    #    board_copy.push(move)
+    #    print(board_copy)
 
