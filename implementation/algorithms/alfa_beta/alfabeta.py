7cbdafb2f61533a9f5fe58981985eee124978e31
Alina <alina.zamfir_1997@yahoo.com>
2018-12-14 22:03:02 +0200

    alfabeta.py reviewed.
    Signed-off-by: Alina <alina.zamfir_1997@yahoo.com>

--- implementation/algorithms/alfa_beta/alfabeta.py
+++ implementation/algorithms/alfa_beta/alfabeta.py
@@ -6,60 +6,75 @@ import utils.chess_utils as chess_functions
 
 
 @typechecked
-def alpha_beta_root(depth : int, board : chess.Board, is_maximizing : bool) -> chess.Move :
-    possible_moves = board.legal_moves
-    best_move = -1
+def alpha_beta_root(depth: int, board: chess.Board, is_maximizing: bool) -> chess.Move:
+    possible_moves  = board.legal_moves
+    best_move       = -1
     best_move_final = None
-    for possible_move in possible_moves :
+    for possible_move in possible_moves:
         move = chess.Move.from_uci(str(possible_move))
         board.push(move)
-        for pos in possible_moves :
+        for pos in possible_moves:
             value = max(best_move, alpha_beta(depth - 1, board, -1, 1, not is_maximizing, pos))
+
         board.pop()
-        if value > best_move :
-            best_move = value
+        if value > best_move:
+            best_move       = value
             best_move_final = move
+
     return best_move_final
 
-@typechecked
-def alpha_beta(depth : int, board : chess.Board, alpha : float, beta : float, is_maximizing : bool, move : chess.Move) -> float:
-    if depth == 0 :
-        return -chess_functions.evaluate_board_state(board, str(move),[chess_functions.DEFEND_PIECES_STRATEGY])
-    possible_moves = board.legal_moves
-    if is_maximizing :
-        best_move = -1
-        for possible_move in possible_moves :
-            move = chess.Move.from_uci(str(possible_move))
-            board.push(move)
-            for pos in possible_moves :
-                best_move = max(best_move, alpha_beta(depth - 1, board, alpha, beta, not is_maximizing, pos))
-            board.pop()
-            alpha = max(alpha, best_move)
-            if beta <= alpha :
-                return best_move
-        return best_move
-    else :
-        best_move = 1
-        for possible_move in possible_moves :
-            move = chess.Move.from_uci(str(possible_move))
-            board.push(move)
-            for pos in possible_moves :
-                best_move = min(best_move, alpha_beta(depth - 1, board, alpha, beta, not is_maximizing, pos))
-            board.pop()
-            beta = min(beta, best_move)
-            if beta <= alpha :
-                return best_move
-        return best_move
 
 @typechecked
-def play() -> None :
+def alpha_beta(depth: int, board: chess.Board, alpha: float, beta: float, is_maximizing: bool, move: chess.Move) -> float:
+    if depth == 0:
+        return -chess_functions.evaluate_board_state(board, str(move),[chess_functions.DEFEND_PIECES_STRATEGY])
+
+    possible_moves = board.legal_moves
+    if is_maximizing:
+        best_move = -1
+        for possible_move in possible_moves:
+            move = chess.Move.from_uci(str(possible_move))
+            board.push(move)
+
+            for pos in possible_moves:
+                best_move = max(best_move, alpha_beta(depth - 1, board, alpha, beta, not is_maximizing, pos))
+
+            board.pop()
+            alpha = max(alpha, best_move)
+
+            if beta <= alpha:
+                return best_move
+
+        return best_move
+    else:
+        best_move = 1
+
+        for possible_move in possible_moves :
+            move = chess.Move.from_uci(str(possible_move))
+            board.push(move)
+
+            for pos in possible_moves :
+                best_move = min(best_move, alpha_beta(depth - 1, board, alpha, beta, not is_maximizing, pos))
+
+            board.pop()
+            beta = min(beta, best_move)
+
+            if beta <= alpha:
+                return best_move
+
+        return best_move
+
+
+@typechecked
+def play() -> None:
     board = chess.Board()
-    n = 0
+    n     = 0
+
     print(board)
-    while not board.is_game_over() :
-        if n % 2 == 0 :
+    while not board.is_game_over():
+        if n % 2 == 0:
             move = alpha_beta_root(2, board, True)
-            print("You'are advised to do this move",move)
+            print("You'are advised to do this move", move)
             move = input("Enter move:")
             move = chess.Move.from_uci(str(move))
             board.push(move)
