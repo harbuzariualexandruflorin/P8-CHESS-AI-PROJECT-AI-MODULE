93fa6d80757de9fcea6393378ff59e48991bfb58
alexgheorghies <alexgheorghies@yahoo.com>
2018-12-15 06:10:16 +0200

    add second json format

--- implementation/output/json_output.py
+++ implementation/output/json_output.py
@@ -8,14 +8,20 @@ import chess
 
 from implementation.algorithms.minmax.minmax import min_max_root as min_max_algorithm
 
+from implementation.algorithms.alfa_beta.alfabeta import alpha_beta_root as alfa_beta_algorithm
 
+from implementation.algorithms.negamax.negamax import negamax_root as negamax_algorithm
 
 @typechecked
 def create_json_main_body(initial_state_FEN : str, variants : list) -> dict :
     return {"initialStateFEN" : initial_state_FEN, "variants" : variants}
 
 @typechecked
-def create_json_variants(algorithm_name : str, strategy_name: str) -> dict :
+def create_second_json_main_body(initial_state_FEN : str, mainVariant : dict) -> dict :
+    return {"initialStateFEN" : initial_state_FEN, "mainVariant" : mainVariant}
+
+@typechecked
+def create_json_variants(algorithm_name : str, strategy_name: list) -> dict :
     return {"moves" : [], "algorithmName" : algorithm_name, "strategyNames" : strategy_name}
 
 @typechecked
@@ -23,14 +29,32 @@ def create_json_information_moves(move : str, score : float) -> dict :
     return {"move" : move, "score" : score}
 
 @typechecked
+def create_json_information_moves_with_variants(move : str, score : float, variants : list) -> dict :
+    return {"move" : move, "score" : score, "variants" : variants}
+
+@typechecked
+def create_json_moves(list_name: list) -> dict :
+    return {"moves" : list_name}
+
+@typechecked
 def add_variants(list_of_moves : list, variants : dict, list_of_information_moves : list) -> None :
     list_of_moves.append(variants)
     list_of_moves[-1]["moves"] = list_of_information_moves
 
 @typechecked
+def add_moves(list_of_moves : list, moves : dict, list_of_information_moves : list) -> None :
+    list_of_moves.append(moves)
+    list_of_moves[-1]["variants"] = list_of_information_moves
+
+@typechecked
 def create_json_output(dictionary) -> None :
     text = json.dumps(dictionary, indent = 4)
     open("output.json","w").write(text)
+
+@typechecked
+def create_json_output(dictionary) -> None :
+    text = json.dumps(dictionary, indent = 4)
+    open("second_output.json","w").write(text)
 
 @typechecked
 def choose_algorithm() -> str :
@@ -63,37 +87,142 @@ def game_strategy(strategy_name : str) -> list :
 def game_algorithm(algorithm_name : str, depth : int, board : chess.Board, is_maximizing : bool, strategy : str) -> chess.Move :
     if algorithm_name == 'MinMax' :
         return min_max_algorithm(depth, board, is_maximizing, game_strategy(strategy))
+    if algorithm_name == 'NegaMax' :
+        return negamax_algorithm(depth, board, game_strategy(strategy))
+    if algorithm_name == 'AlfaBeta' :
+        return alfa_beta_algorithm(depth, board, is_maximizing, game_strategy(strategy))
 
 @typechecked
-def play(initial_state_FEN : str) -> None :
-    board = chess.Board()
-    print(board)
-    list_moves=list()
-    while not board.is_game_over() :
-        if board.turn :
-            algorithm=choose_algorithm()
-            strategy=choose_strategy()
-            variants = create_json_variants(algorithm, strategy)
-            move = game_algorithm(algorithm, 1, board, True, strategy)
-            score = chess_functions.evaluate_board_state(board, str(move), game_strategy(strategy))
-            information_move = create_json_information_moves('w'+str(move), score)
-            add_variants(list_moves, variants, [information_move])
-            print("You are advised to do this move",move)
-            move = input("Enter move:")
-            move = chess.Move.from_uci(str(move))
-            board.push(move)
-        else :
-            print("Computer's Turn:")
-            variants = create_json_variants("MinMax", "PAWN_ADVANCE_STRATEGY")
-            move = min_max_algorithm(1, board, True, [chess_functions.PAWN_ADVANCE_STRATEGY])
-            score = chess_functions.evaluate_board_state(board, str(move), [chess_functions.PAWN_ADVANCE_STRATEGY])
-            information_move = create_json_information_moves('b'+str(move), score)
-            add_variants(list_moves, variants, [information_move])
-            move = chess.Move.from_uci(str(move))
-            board.push(move)
+def possible_variants(initial_state_FEN : str) -> None :
+    list_of_algorithms = ['MinMax', 'NegaMax', 'AlfaBeta']
+    strategy = choose_strategy()
+    list_moves = list()
+    for algorithm in list_of_algorithms :
+        board = chess.Board(initial_state_FEN)
         print(board)
+        variants = create_json_variants(algorithm, [strategy])
+        print(algorithm)
+        n = 0
+        while n != 2 :
+            if board.turn :
+                n += 1
+                move = game_algorithm(algorithm, 1, board, True, strategy)
+                score = chess_functions.evaluate_board_state(board, str(move), game_strategy(strategy))
+                information_move = create_json_information_moves('w'+str(move), score)
+                add_moves(list_moves, variants, [information_move])
+                #print("You are advised to do this move",move)
+                #move = input("Enter move:")
+                move = chess.Move.from_uci(str(move))
+                board.push(move)
+            else :
+                n += 1
+                #print("Computer's Turn:")
+                move = game_algorithm(algorithm, 1, board, True, strategy)
+                score = chess_functions.evaluate_board_state(board, str(move), game_strategy(strategy))
+                information_move = create_json_information_moves('b'+str(move), score)
+                list_moves[-1]['moves'].append(information_move)
+                move = chess.Move.from_uci(str(move))
+                board.push(move)
+            print(board)
         json_main_part = create_json_main_body(initial_state_FEN, list_moves)
         create_json_output(json_main_part)
 
+list_all_moves = list()
+list_all_scores = list()
+
+@typechecked
+def play() -> None :
+    global list_all_moves
+    global list_all_scores
+    board = chess.Board()
+    print(board)
+    n = 0
+    while not board.is_game_over() :
+        if board.turn :
+            n += 1
+            move = min_max_algorithm(1, board, True, [chess_functions.PAWN_ADVANCE_STRATEGY])
+            score = chess_functions.evaluate_board_state(board, str(move), [chess_functions.PAWN_ADVANCE_STRATEGY])
+            print(score)
+            list_all_scores.append(score)
+            print("You are advised to do this move",move)
+            #move = input("Enter move:")
+            move = chess.Move.from_uci(str(move))
+            list_all_moves.append(str(move))
+            board.push(move)
+        else :
+            n += 1
+            print("Computer's Turn:")
+            move = min_max_algorithm(1, board, True, [chess_functions.PAWN_ADVANCE_STRATEGY])
+            score = chess_functions.evaluate_board_state(board, str(move), [chess_functions.PAWN_ADVANCE_STRATEGY])
+            print(score)
+            list_all_scores.append(score)
+            move = chess.Move.from_uci(str(move))
+            list_all_moves.append(str(move))
+            board.push(move)
+        print(board)
+        print(list_all_scores,list_all_moves)
+        print(n)
+        #if n > 3 :
+         #   break
+
+@typechecked
+def get_board(move_number_n : int ,initial_state_FEN : str) -> chess.Board :
+    board = chess.Board(initial_state_FEN)
+    for number_move in range(0, move_number_n-1) :
+        move = chess.Move.from_uci(list_all_moves[number_move])
+        board.push(move)
+    return board
+
+@typechecked
+def possible_moves_instead_of_wrong_move(move_number_n : int ,initial_state_FEN : str, board_state : chess.Board) -> None:
+    if move_number_n % 2 == 0 :
+        first_color_piece, second_color_piece = 'w', 'b'
+    else :
+        first_color_piece, second_color_piece = 'b', 'w'
+    first_move, second_move = first_color_piece+list_all_moves[move_number_n - 1], second_color_piece+list_all_moves[move_number_n]
+    first_score, second_score = list_all_scores[move_number_n - 1], list_all_scores[move_number_n]
+    list_of_algorithms = ['MinMax', 'NegaMax', 'AlfaBeta']
+    strategy = choose_strategy()
+    #strategy='PAWN_ADVANCE_STRATEGY'
+    list_moves = list()
+    for algorithm in list_of_algorithms :
+        board = board_state
+        print(board)
+        variants = create_json_variants(algorithm, [strategy])
+        print(algorithm)
+        print(board.turn)
+        n = 0
+        while n != 2 :
+            if not board.turn :
+                n += 1
+                move = game_algorithm(algorithm, 1, board, True, strategy)
+                score = chess_functions.evaluate_board_state(board, str(move), game_strategy(strategy))
+                print(move, score)
+                information_move = create_json_information_moves(first_color_piece+str(move), score)
+                print(information_move)
+                add_variants(list_moves, variants, [information_move])
+                #print("You are advised to do this move",move)
+                #move = input("Enter move:")
+                move = chess.Move.from_uci(str(move))
+                board.push(move)
+            else :
+                n += 1
+                #print("Computer's Turn:")
+                move = game_algorithm(algorithm, 1, board, True, strategy)
+                score = chess_functions.evaluate_board_state(board, str(move), game_strategy(strategy))
+                information_move = create_json_information_moves(second_color_piece+str(move), score)
+                list_moves[-1]['moves'].append(information_move)
+                move = chess.Move.from_uci(str(move))
+                board.push(move)
+            print(board)
+    d1=create_json_information_moves_with_variants(first_move,first_score,list_moves)
+    d2=create_json_information_moves_with_variants(second_move,second_score,list_moves)
+    dictionary=create_json_moves([d1,d2])
+    json_main_part = create_second_json_main_body(initial_state_FEN, dictionary)
+    create_json_output(json_main_part)
+
 if __name__ == "__main__" :
-    play("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    #possible_variants("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    play()
+    possible_moves_instead_of_wrong_move(2,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",get_board(2,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+    #print(get_board(2,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
