a3b97ed7dcfba6915bd3c748ff7fadc10a302c0c
harbuzariualexandruflorin <harbuzariualexandruflorin@gmail.com>
2018-12-16 21:09:52 +0200

    json fix

--- webapi/services.py
+++ webapi/services.py
@@ -87,98 +87,95 @@ def generate_response_case1(fen: str) -> str:
 
 
 @typechecked
-def generate_response_case2(fen: str, list: list) -> str:
-    depth = 1
-    board = chess.Board(fen)
-    bad_move = mistake_move(list.copy(), len(list) % 2 + 1, 0)
-    list_for_min = []
-    list_for_alfa = []
-    list_for_nega = []
-    for index in range(0, len(list)):
-        if index == bad_move[1]:
-            negamax_board = board.copy()
-            minmax_board = board.copy()
-            alphabeta_bord = board.copy()
-            i = 0
-            while i < 6:
+def generate_response_case2(fen : str, list : list) -> str:
+    depth=1
+    fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    board=chess.Board(fen)
+    bad_move=mistake_move(list.copy(),len(list)%2+1,0)
+    list_for_min=[]
+    list_for_alfa=[]
+    list_for_nega=[]
+    for index in range (0, len(list)):
+        if index==bad_move[1]:
+            board2=board.copy()
+            for p in range(0,index-1):
+                board2.push_uci(list[p])
+            negamax_board=board2.copy()
+            minmax_board=board2.copy()
+            alphabeta_bord=board2.copy()
+            i=0
+            while i<6:
                 with Pool(processes=4) as pool:
                     rminmax = pool.apply_async(minmax_algorithm, (depth, minmax_board, True,))
                     rnegamax = pool.apply_async(negamax_algorithm, (depth + 1, negamax_board,))
                     ralfabeta = pool.apply_async(alphabeta_algorihm, (depth, alphabeta_bord, True,))
-                    movealfa, valuealfa = ralfabeta.get()
-                    movemin, valuemin = rminmax.get()
-                    movenega, valuenega = rnegamax.get()
-                    list_for_alfa.append((str(movealfa), valuealfa))
-                    list_for_min.append((str(movemin), valuemin))
-                    list_for_nega.append((str(movenega), valuenega))
+                    movealfa,valuealfa=ralfabeta.get()
+                    movemin,valuemin=rminmax.get()
+                    movenega,valuenega=rnegamax.get()
+                    list_for_alfa.append((str(movealfa),valuealfa))
+                    list_for_min.append((str(movemin),valuemin))
+                    list_for_nega.append((str(movenega),valuenega))
                     negamax_board.push(movenega)
                     minmax_board.push(movemin)
                     alphabeta_bord.push(movealfa)
-                i += 1
+                i+=1
     color = ["w", "b"]
     color_pos = 0
+    poz = 0
     if "w" in fen:
-        color_pos = 0
+        color_pos=0
+        poz = 0
     else:
-        color_pos = 1
-    algorithms = [("MinMax", list_for_min, "PAWN_ADVANCE_STRATEGY"), ("NegaMax", list_for_nega, "TAKE_PIECES_STRATEGY"),
-                  ("AlfaBeta", list_for_alfa, "DEFEND_PIECES_STRATEGY")]
-    list_moves = []
-    list_all_moves = []
+        color_pos=1
+        poz = 1
+    algorithms=[("MinMax",list_for_min,"PAWN_ADVANCE_STRATEGY"),("NegaMax",list_for_nega,"TAKE_PIECES_STRATEGY"),("AlfaBeta",list_for_alfa,"DEFEND_PIECES_STRATEGY")]
+    list_moves=[]
+    list_all_moves=[]
     list_all_variants = []
-    list_strategies = [chess_functions.PAWN_ADVANCE_STRATEGY, chess_functions.DEFEND_PIECES_STRATEGY,
-                       chess_functions.TAKE_PIECES_STRATEGY, chess_functions.ATTACK_PIECES_STRATEGY,
-                       chess_functions.KEEP_PIECES_STRATEGY]
-    for move in list:
-        if move in bad_move:
+    list_strategies=[chess_functions.PAWN_ADVANCE_STRATEGY,chess_functions.DEFEND_PIECES_STRATEGY,chess_functions.TAKE_PIECES_STRATEGY,chess_functions.ATTACK_PIECES_STRATEGY,chess_functions.KEEP_PIECES_STRATEGY]
+    for move in list :
+        if move in bad_move :
             n = 0
             for algorithm in algorithms:
-                variants = output.create_json_variants(algorithm[0], [algorithm[2]])
+                variants = output.create_json_variants(algorithm[0],[algorithm[2]])
                 n += 1
-                m = 0
-                for move_algorithm, score in algorithm[1]:
-                    if color_pos % 2 == 0:
-                        m += 1
-                        information_move = output.create_json_information_moves(color[color_pos] + move_algorithm,
-                                                                                score)
-                        # print("Info White",information_move)
+                m=0
+                for move_algorithm,score in algorithm[1]:
+                    if color_pos%2==0:
+                        m+=1
+                        information_move=output.create_json_information_moves(color[1-color_pos]+move_algorithm,score)
+                        #print("Info White",information_move)
                         #                       output.add_variants(list_moves,variants,[information_move])
-                        if m == 1:
+                        if m==1 :
                             output.add_variants(list_moves, variants, [information_move])
                         else:
                             list_moves[-1]['moves'].append(information_move)
 
                     else:
-                        m += 1
-                        information_move = output.create_json_information_moves(color[color_pos] + move_algorithm,
-                                                                                score)
-                        # list_moves[-1]['moves'].append(information_move)
-                        if m == 1:
+                        m+=1
+                        information_move=output.create_json_information_moves(color[1-color_pos]+move_algorithm,score)
+                        #list_moves[-1]['moves'].append(information_move)
+                        if m==1 :
                             output.add_variants(list_moves, variants, [information_move])
                         else:
                             list_moves[-1]['moves'].append(information_move)
-                        # print("Info Black",information_move)
-                    color_pos = 1 - color_pos
+                        #print("Info Black",information_move)
+                    color_pos = 1-color_pos
 
                 list_all_variants.append(variants)
-                # print(list_all_variants)
-                if n == 3:
-                    d = output.create_json_moves(list_all_variants)
-                    list_all_moves.append(output.create_json_information_moves_with_variants(move,
-                                                                                             chess_functions.evaluate_board_state(
-                                                                                                 board, move,
-                                                                                                 list_strategies),
-                                                                                             list_moves))
+                #print(list_all_variants)
+                if n == 3 :
+                    d=output.create_json_moves(list_all_variants)
+                    list_all_moves.append(output.create_json_information_moves_with_variants(color[poz]+move,chess_functions.evaluate_board_state(board, move, list_strategies),list_moves))
 
         else:
-            list_all_moves.append(output.create_json_information_moves(move,
-                                                                       chess_functions.evaluate_board_state(board, move,
-                                                                                                            list_strategies)))
+            list_all_moves.append(output.create_json_information_moves(color[poz]+move,chess_functions.evaluate_board_state(board, move, list_strategies)))
+        poz=1-poz
         board.push_uci(move)
-    # d2=output.create_json_information_moves_with_variants(second_move,second_score,list_moves)
-    # print(list_all_moves)
-    dictionary = output.create_json_moves(list_all_moves)
-    # dictionary={"moves":list_all_moves}
+        #d2=output.create_json_information_moves_with_variants(second_move,second_score,list_moves)
+    #print(list_all_moves)
+    dictionary=output.create_json_moves(list_all_moves)
+    #dictionary={"moves":list_all_moves}
     json_main_part = output.create_second_json_main_body(fen, dictionary)
     output.create_json_output2(json_main_part)
     return convert_json_to_string('second_output.json')
